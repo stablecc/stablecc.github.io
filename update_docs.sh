be482b70a1d3fc33bc05b86ead6ae8cf802840dc
Mike Wilson <mike@stablecc.com>
2022-03-27 16:25:14 -0700

    update docs

--- update_docs.sh
+++ update_docs.sh
@@ -19,3 +19,7 @@ rm -rf scclib-sqlite-doxygen/*
 cp -r ../scclib-sqlite/docs/html/* scclib-sqlite-doxygen/
 mkdir scclib-sqlite-doxygen/lic
 cp ../scclib-sqlite/lic/* scclib-sqlite-doxygen/lic/
+
+set -x
+echo 'best bet is git add -u followed by git add *'
+printf '\xf0\x9f\x99\x82\n'
